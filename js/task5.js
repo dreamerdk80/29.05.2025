aca554a576f443ffc7125f0a4735fcc8743be3e9
dreamerdk80 <dreamer_dk@mail.ru>
2025-07-02 22:15:33 +0300

    task5

---
+++ js/task5.js
@@ -0,0 +1,11 @@
+const mainPicture = document.querySelector(".main-picture")
+const pictures = document.querySelectorAll(".picture")
+
+pictures.forEach(picture => {
+    picture.addEventListener("click", () => {
+        mainPicture.src = picture.src
+        mainPicture.alt = picture.alt
+
+        console.log(mainPicture.src)
+    })
+})
